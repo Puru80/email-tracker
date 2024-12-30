6e2802e6bcf42b627a0171c183d55d9f3acd1d85
Puru80 <puru.agar99@gmail.com>
2024-12-30 15:42:05 +0530

    solving email read issue

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ def health():
 
 @app.route("/track/<unique_id>", methods=["GET"])
 def track_email(unique_id):
-    
+
     ip_address = request.remote_addr
     print("IP address: ", ip_address)
+
+    email_headers = request.headers
+    print("Email headers: ", email_headers)
 
     print("Tracking email with unique_id: ", unique_id)
     tracking_satus = db.track_email(unique_id)
