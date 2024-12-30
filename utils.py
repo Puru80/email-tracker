d7cfa2e79f32c2c60a20d9dd8758afc106e24f32
Puru80 <puru.agar99@gmail.com>
2024-12-30 17:40:02 +0530

    testing client IP

--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
 import json
 
-class Utils: 
-    
+
+class Utils:
+
     def __init__(self, config_file):
         with open(config_file, "r") as f:
             config = json.load(f)
 
-        self.read_ignore_ips = config["read_ignore_ips"]
+        self.read_ignore_ips = config["read_ignore_ips"].split(", ")
 
     def is_ip_ignored(self, client_ip):
         if client_ip in self.read_ignore_ips:
             print("Ignoring IP: ", client_ip)
             return True
-        
+
         return False
