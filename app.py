4f5781ce2c8c97619fd439a57396fdeaf5100a28
Puru80 <puru.agar99@gmail.com>
2024-12-30 16:53:02 +0530

    getting client IP

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from database import Database
+from utils import Utils
 
 app = Flask(__name__)
 
 db = Database("config.json")
 db.create_tables()
+
+utils = Utils("config.json")
 
 
 @app.route("/health", methods=["GET"])
@@ -20,8 +23,11 @@ def track_email(unique_id):
     email_headers = request.headers
     print("Email headers: ", email_headers)
     
-    meta = request.access_control_request_headers
-    print("Meta: ", meta)
+    client_ip = email_headers.get("True-Client-Ip")
+    print("Client IP: ", client_ip)
+    
+    if utils.is_ip_ignored(client_ip): 
+        return "IP ignored"
     
     print("Tracking email with unique_id: ", unique_id)
     tracking_satus = db.track_email(unique_id)
